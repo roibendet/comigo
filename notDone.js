aa8bea34168972baa51b41d569cf60deb11408b7
Roi Bendet <roi_bendet@yahoo.com>
2017-05-16 13:18:19 +0300

    finalversion

---
+++ notDone.js
@@ -0,0 +1,69 @@
+<!--<section class="learn-more-about-comigo clearfix">
+  <div class="learn-more-about-comigo-left">
+  <div class="learn-more-about-comigo-left-up">
+  <div class="learn-more-about-comigo-left-up-1">
+
+  <h3>learn more about comigo</h3>
+
+</div>
+<div class="learn-more-about-comigo-left-up-2">
+  <a href=""> <span>Download our brochure to learn more </span></a>
+</div>
+</div>
+<div class="learn-more-about-comigo-left-bottom">
+  <div class="social">
+  <ul>
+  <li><a href="#">
+  <img src="" alt="faceook" title="visit us on facebook">
+  </a></li>
+
+  <li><a href="#">
+  <img src="#" alt="twitter" title="visit us on Twitter">
+  </a></li>
+
+  <li><a href="#">
+  <img src="" alt="linkedin" title="visit us on linkedin">
+  </a></li>
+
+  <li><a href="#">
+  <img src="" alt="youtube" title="visit us on youtube">
+  </a></li>
+  </ul>
+  </div>
+  <div class="schedule">
+  <a href=""><span>schedule a meeting at ibc 2016</span></a>
+</div>
+</div>
+</div>
+
+<div class="learn-more-about-comigo-right">
+
+
+  <a class="demo" href="#"><span>schedule a demo</span></a>
+
+
+</div>
+</section>
+<section class="blog clearfix">
+  <ul class="blog-ul clearfix">
+  <li>
+  <a class="ourblog" href="#">
+  <div class="ourblogdiv">
+  <h3>our <br> blog</h3>
+  <p>the inside view</p>
+</div>
+</a>
+
+</li>
+<li><a
+href="#"><span>Comigo's Experience Intelligence Products Redefine TV - And You Can Find Out How at IBC2016</span></a>
+</li>
+<li><a href="#"><span>What We Learned at TVConnect 2016</span></a></li>
+<li><a href="#"><span>Big Data Meets TV - The Next Phase In The TV Experience Evolution</span></a></li>
+<li><a
+href="#"><span>Comigo Will Bring an Infusion of Internet Power and Contextual TV to Life at TV Connect 2016</span></a>
+</li>
+<li><a href="#"><span>Quality, Set-Top-Boxes, Crappy Cars, and Caveat Emptor</span></a></li>
+</ul>
+
+</section>-->
